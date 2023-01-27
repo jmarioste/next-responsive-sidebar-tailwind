422a50cbae486a7b0edd87cf1e0ae68296c62a3e
Jasser Mark Arioste <jassermark.arioste@gmail.com>
2023-01-27 18:22:12 +0800

    Implement navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,14 @@
 import React from "react";
-
+import { Bars3Icon } from "@heroicons/react/24/outline";
 const Navbar = () => {
   return (
-    <div className="w-full bg-gray-800 h-16 text-zinc-50 fixed z-10">
-      Navbar
-    </div>
+    <nav className="w-full bg-gray-800 h-16 text-zinc-50 fixed z-10 flex items-center px-4">
+      <div className="font-bold text-lg">My Logo</div>
+      <div className="flex-grow"></div>
+      <button className="md:hidden">
+        <Bars3Icon className="h-6 w-6" />
+      </button>
+    </nav>
   );
 };
 
